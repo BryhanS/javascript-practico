f6fe836f04ab46e6e2b287f1652462436cadd693
bryhans <bryhansalazarpacsi@gmail.com>
2022-11-29 15:28:39 -0500

    addeventlistener

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-// h1 { color:red} similar a css
-
-
 
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
-
-console.log(input)
-
-console.log({
-  h1,
-  p,
-  parrafito,
-  pid,
-  input
-})
-
-h1.innerHTML = 'Papito <br> feo';
-h1.innerText = 'Papito <br> feo';
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
 
-input.value = "456"
-
-
-const img = document.createElement('img');
-
-img.setAttribute('src','https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Platzi.jpg/1200px-Platzi.jpg');
-
-console.log(img);
-
-pid.append(img);
+function btnOnClick(){
+  
+  let sumaInputs = input1.value + input2.value
+  pResult.innerText = "Resultado: " +sumaInputs;
+}
